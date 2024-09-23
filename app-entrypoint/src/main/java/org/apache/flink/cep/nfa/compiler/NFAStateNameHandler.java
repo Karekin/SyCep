ada7f195dd0407612657b7c17b2e2c9d850a18fb
v_lijinzhong_dxm <v_lijinzhong@duxiaoman.com>
2024-09-23 19:39:26 +0800

    01_nfa

--- app-entrypoint/src/main/java/org/apache/flink/cep/nfa/compiler/NFAStateNameHandler.java
+++ app-entrypoint/src/main/java/org/apache/flink/cep/nfa/compiler/NFAStateNameHandler.java
@@ -18,19 +18,19 @@
 
 package org.apache.flink.cep.nfa.compiler;
 
-import org.apache.flink.cep.pattern.MalformedPatternException;
-import org.apache.flink.util.Preconditions;
-
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.flink.cep.pattern.MalformedPatternException;
+import org.apache.flink.util.Preconditions;
+
 /**
  * A utility class used to handle name conventions and guarantee unique names for the states of our
- * {@link org.apache.flink.cep.nfa.NFA}.
+ * {@link cepnfa.NFA}.
  */
 public class NFAStateNameHandler {
 
-    private static final String STATE_NAME_DELIM = ":";
+    public static final String STATE_NAME_DELIM = ":";
 
     private final Set<String> usedNames = new HashSet<>();
 
@@ -65,7 +65,7 @@ public class NFAStateNameHandler {
     }
 
     /**
-     * Used to give a unique name to {@link org.apache.flink.cep.nfa.NFA} states created during the
+     * Used to give a unique name to {@link cepnfa.NFA} states created during the
      * translation process. The name format will be {@code baseName:counter} , where the counter is
      * increasing for states with the same {@code baseName}.
      *
